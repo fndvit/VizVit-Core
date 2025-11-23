f3de6b15c9a72715856591ca511e87500225565c
LeVarez <aleixac1997@gmail.com>
2025-11-23 21:05:45 +0100

    update export

--- src/virtual-datastore/index.ts
+++ src/virtual-datastore/index.ts
@@ -1,5 +1,6 @@
 export { Database } from "./db/VizVitDB.js";
 
+export { Database as DatabaseBeta } from "./db/database.js";
 export type { Dataset, Row } from "./types/index.js";
 
 export type { BarChartItem } from "./bridge/barchartBridge.js";
